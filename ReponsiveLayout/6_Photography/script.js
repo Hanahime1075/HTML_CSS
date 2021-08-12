9acd649849cb6dcb661c64df0a092d759f744e66
Khuong1075 <ngduykhuong0503@gmail.com>
2021-08-12 10:56:36 +0700

    clone plural and blog web Traversy Media

---
+++ ReponsiveLayout/6_Photography/script.js
@@ -0,0 +1,11 @@
+//animatie smooth scroll
+$("#view-work").on("click", function () {
+  const images = $("#images").position().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: images,
+    },
+    900
+  );
+});
